43bdcec9d83fea0a9d5dc898047bd5bae52967d8
Dan Hitchcock <33898935+lunaautomation@users.noreply.github.com>
2019-07-26 06:36:53 +0100

    custom commands added

--- cypress/integration/UnderTest.spec.js
+++ cypress/integration/UnderTest.spec.js
@@ -3,19 +3,15 @@ describe('Test', () => {
 
         cy.visit('https://www.nationwide.co.uk');
         cy.title().should('include', 'Nationwide Building Society')
-        
-        cy.get('#MortgagesNavItem').trigger('mouseover');
-        cy.get('#MortgagesNavItem').contains('Mortgage rates').click({ force: true });
-        
-        cy.get("#selectorItemHaveNationwideMortgage1").click();
-        cy.get("#selectorItemNationwideMortgageTypeNo2").click();
-        cy.get("#SearchPropertyValue").type("300000");
-        cy.get("#SearchMortgageAmount").type("150000");
-        cy.get('#SearchMortgageTerm').type("30");
-        cy.get('#myButton').click();
+
+        cy.selectFromMortgages('Mortgage rates');
+        cy.hasNationwideMortgage(false);
+        cy.customerType('changing');
+        cy.mortgage_values('300000','150000','30');
+
         cy.get('#product-fee-fee').click();
         cy.get('#fixed').click();
-      
+
         cy.get('div.ratesTableWrapper._2yr').should('be.visible')
         cy.get('div.ratesTableWrapper._3yr').should('be.visible')
         cy.get('div.ratesTableWrapper._5yr').should('be.visible')
--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -23,3 +23,44 @@
 //
 // -- This is will overwrite an existing command --
 // Cypress.Commands.overwrite("visit", (originalFn, url, options) => { ... })
+
+Cypress.Commands.add("mortgage_values", (propValue, morValue, termLength) => {
+    cy.get("#SearchPropertyValue").type(propValue);
+    cy.get("#SearchMortgageAmount").type(morValue);
+    cy.get('#SearchMortgageTerm').type(termLength);
+    cy.get('#myButton').click();
+});
+
+Cypress.Commands.add("selectFromSavings", (menuItem) => {
+    cy.get('#SavingsNavItem').contains(menuItem).click({ force: true });
+});
+
+Cypress.Commands.add("selectFromMortgages", (menuItem) => {
+    cy.get('#MortgagesNavItem').contains(menuItem).click({ force: true });
+});
+
+Cypress.Commands.add("hasNationwideMortgage", (answer) => {
+    if(answer)
+    {
+    cy.get("#selectorItemHaveNationwideMortgage0").click();
+    }
+    else
+    {
+    cy.get("#selectorItemHaveNationwideMortgage1").click();
+    }
+});
+
+Cypress.Commands.add("customerType", (value) => {
+    if (value === "new")
+    {
+        cy.get("#selectorItemNationwideMortgageTypeNo0").click();
+    }
+    else if (value === "moving")
+    {
+        cy.get("#selectorItemNationwideMortgageTypeNo1").click();
+    }
+    else if (value === "changing")
+    { 
+        cy.get("#selectorItemNationwideMortgageTypeNo2").click();
+    }
+});
